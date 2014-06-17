52a6b93b5be0142848db9023c413b9b8f48883f0
Charles Henrotte <charles.henrotte@enst.fr>
2014-06-17 11:43:22 +0200

    First Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try{
+			UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
+			userAgent.visit("http://www.infres.enst.fr/wp/general-information/annuaire/");                        //visit a url  
+			System.out.println(userAgent.doc.innerHTML());               //print the document as HTML
+		}
+		catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+			System.err.println(e);
+		}
 
 	}
 
